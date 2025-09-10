3421fb32546c595b835dd6b94d62b261f4622419
awakening <sunlightlucid@gmail.com>
2025-09-10 12:59:43 +0700

    loader/ee1: fix it after cmake migration
    - remove retrying
    
    - remove embedded payload sizes and checksums, fetch it from modchip cmd instead
    
    - use crc32 checksum instead of fletcher16 to take advantage of dma sniffer

--- fruitchip-loader/ee-stage1/src/main.c
+++ fruitchip-loader/ee-stage1/src/main.c
@@ -2,6 +2,17 @@
 #include <sio.h>
 
 #include <modchip/io.h>
+#include <modchip/cmd.h>
+#include "crc32.h"
+
+inline static void modchip_cmd_or_panic(u32 cmd)
+{
+    if (!modchip_cmd(cmd))
+    {
+        sio_puts("EE1: cmd failed");
+        asm volatile("break\n");
+    }
+}
 
 void __attribute__((section(".entry"))) __ExecPS2(void* entry, void* gp, int argc, char** argv)
 {
@@ -10,46 +21,25 @@ void __attribute__((section(".entry"))) __ExecPS2(void* entry, void* gp, int arg
     (void)argc; // 1
     (void)argv; // rom0:OSDSYS
 
-    // u16 checksum = fletcher16((void *)EE_STAGE_1_ADDR, EE_STAGE_1_SIZE);
-    // if (checksum == EE_STAGE_1_CHECKSUM)
-    //     sio_puts("EE1: self-check ok");
-    // else
-    //     sio_puts("EE1: self-check bad");
+    modchip_cmd_or_panic(MODCHIP_CMD_GET_EE_STAGE1_SIZE);
+    u32 ee_stage1_size = modchip_peek_u32();
 
-    u32 try = 0;
-    while (true)
-    {
-        if (try > 4)
-        {
-            sio_puts("EE1: hit retry limit");
-            asm volatile("break\n");
-        }
+    u32 ee_stage1_crc = *(uiptr *)(EE_STAGE_1_ADDR + ee_stage1_size);
+    u32 crc = crc32((void *)EE_STAGE_1_ADDR, ee_stage1_size);
+    ee_stage1_crc == crc ? sio_puts("EE1: self-check ok") : sio_puts("EE1: self-check bad");
 
-        sio_puts("EE1: cmd");
-        modchip_poke_u8(0xC0);
-        modchip_poke_u8(0xCB);
+    modchip_cmd_or_panic(MODCHIP_CMD_GET_EE_STAGE2_SIZE);
+    u32 ee_stage2_size = modchip_peek_u32();
 
-        sio_puts("EE1: reading");
-        // for (uiptr i = 0; i < EE_STAGE_2_SIZE; i += 4)
-        //     *(volatile u32 *)(EE_STAGE_2_ADDR + i) = modchip_peek();
+    // payload seems to be aligned to 4 bytes anyway, and even if its not,
+    // extra zeroes at the end should still be fine as long as we're under size limit
+    modchip_cmd_or_panic(MODCHIP_CMD_GET_EE_STAGE2);
+    for (uiptr i = 0; i < ee_stage2_size; i += 4)
+        *(volatile u32 *)(EE_STAGE_2_ADDR + i) = modchip_peek_u32();
 
-        // u16 checksum = fletcher16((void *)EE_STAGE_2_ADDR, EE_STAGE_2_SIZE);
-        // if (checksum == 0x0000)
-        // {
-        //     sio_puts("EE1: read fail");
-        //     asm volatile("break\n");
-        // }
-
-        // if (checksum != EE_STAGE_2_CHECKSUM)
-        // {
-        //     sio_puts("EE1: read bad");
-        //     try += 1;
-        //     continue;
-        // }
-
-        sio_puts("EE1: read ok");
-        break;
-    }
+    u32 ee_stage2_crc = modchip_peek_u32();
+    crc = crc32((void *)EE_STAGE_2_ADDR, ee_stage2_size);
+    ee_stage2_crc == crc ? sio_puts("EE1: read ok") : sio_puts("EE1: read bad");
 
     FlushCache(0); // flush data cache
     FlushCache(2); // invalidate instruction cache
