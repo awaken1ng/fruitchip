2a3ba4104a6a51ce89575ce330f81d2d64d13ebb
awakening <sunlightlucid@gmail.com>
2025-11-14 18:30:25 +0700

    fw: avoid extra flash reads in read app command handler
    running wth XIP cache disabled, which to be fair was the worst case scenario, showed that status code response is not getting sent fast enough
    
    to avoid commands randomly failing (presumbly due to XIP misses), move stuff that called into flash before actual command handling

--- fruitchip-fw/src/boot_rom/write/read_app.c
+++ fruitchip-fw/src/boot_rom/write/read_app.c
@@ -15,22 +15,26 @@ static uint8_t app_idx = 0;
 static uint8_t app_idx_xor = 0;
 static bool with_crc = 0;
 
+static bool is_valid_apps_partition = false;
+static bool is_valid_read_offset = false;
+static bool is_valid_read_size = false;
+static bool is_valid_app_idx = false;
+
+static uint8_t *read_addr = 0;
+
 static void __time_critical_func(read_apps_partition)(uint8_t w)
 {
     // cmd_byte_counter = 23
 
     uint8_t with_crc_xor = w;
 
-    bool is_valid_read_offset = (read_offset ^ read_offset_xor) == 0xFFFFFFFF && apps_partition_entry_length(app_idx)  > read_offset;
-    bool is_valid_read_size = (read_size ^ read_size_xor) == 0xFFFFFFFF;
-    bool is_valid_app_idx = (app_idx ^ app_idx_xor) == 0xFF && apps_partition_entries_count() > app_idx;
+    // avoid any extra flash reads here, if XIP cache misses, we might end up too late to respond with a status code
     bool is_valid_with_crc = (with_crc ^ with_crc_xor) == 0xFF;
-    bool is_valid = apps_partition_detect() && is_valid_read_offset && is_valid_read_size && is_valid_app_idx && is_valid_with_crc;
+    bool is_valid = is_valid_apps_partition && is_valid_read_offset && is_valid_read_size && is_valid_app_idx && is_valid_with_crc;
 
     if (is_valid)
     {
-        uint8_t *addr = apps_partition_entry_addr(app_idx) + read_offset;
-        boot_rom_data_out_start_data_with_status_code(MODCHIP_CMD_RESULT_OK, addr, read_size, with_crc);
+        boot_rom_data_out_start_data_with_status_code(MODCHIP_CMD_RESULT_OK, read_addr, read_size, with_crc);
     }
     else
     {
@@ -51,25 +55,35 @@ void __time_critical_func(handle_write_read_apps_partition)(uint8_t w)
         case 4: read_offset = w; break;
         case 5: read_offset |= w << 8; break;
         case 6: read_offset |= w << 16; break;
-        case 7: read_offset |= w << 24; break;
+        case 7: read_offset |= w << 24;
+            is_valid_apps_partition = apps_partition_detect();
+            break;
 
         case 8: read_offset_xor = w; break;
         case 9: read_offset_xor |= w << 8; break;
         case 10: read_offset_xor |= w << 16; break;
-        case 11: read_offset_xor |= w << 24; break;
+        case 11: read_offset_xor |= w << 24;
+            is_valid_read_offset = (read_offset ^ read_offset_xor) == 0xFFFFFFFF && apps_partition_entry_length(app_idx)  > read_offset;
+            break;
 
         case 12: read_size = w; break;
         case 13: read_size |= w << 8; break;
         case 14: read_size |= w << 16; break;
-        case 15: read_size |= w << 24; break;
+        case 15: read_size |= w << 24;
+            break;
 
         case 16: read_size_xor = w; break;
         case 17: read_size_xor |= w << 8; break;
         case 18: read_size_xor |= w << 16; break;
-        case 19: read_size_xor |= w << 24; break;
+        case 19: read_size_xor |= w << 24;
+            is_valid_read_size = (read_size ^ read_size_xor) == 0xFFFFFFFF;
+            break;
 
         case 20: app_idx = w; break;
-        case 21: app_idx_xor = w; break;
+        case 21: app_idx_xor = w;
+            is_valid_app_idx = (app_idx ^ app_idx_xor) == 0xFF && apps_partition_entries_count() > app_idx;
+            read_addr = apps_partition_entry_addr(app_idx) + read_offset;
+            break;
 
         case 22:
             with_crc = w;
