7e617446c6de2bc7dd13c3126de94d414de9863d
awakening <sunlightlucid@gmail.com>
2025-12-06 14:28:33 +0700

    menu: add support for multiple message scene

--- fruitchip-menu/src/scene/message.c
+++ fruitchip-menu/src/scene/message.c
@@ -10,17 +10,50 @@
 #include "scene/message.h"
 #include "constants.h"
 
-static struct scene_state_message
-{
+typedef struct message_state {
     wchar_t *text;
     float y;
-} scene_state;
+} message_state_t;
+
+void message_state_init(message_state_t *obj)
+{
+    obj->text = NULL;
+    obj->y = 0;
+}
+
+void message_state_copy(message_state_t *obj, const message_state_t *src)
+{
+    message_state_init(obj);
+
+    if (src->text)
+        obj->text = wcsdup(src->text);
+
+    obj->y = src->y;
+}
+
+void message_state_free(message_state_t *obj)
+{
+    free(obj->text);
+}
+
+ARRAY_DEF(
+    array_message_state,
+    message_state_t,
+    (
+        INIT(API_2(message_state_init)),
+        INIT_SET(API_6(message_state_copy)),
+        SET(API_6(message_state_copy)),
+        CLEAR(API_2(message_state_free)),
+    )
+)
+
+static array_message_state_t scene_state;
 
 static void scene_input_handler_message(struct state *state, int input)
 {
     if (input & PAD_CIRCLE)
     {
-        free(scene_state.text);
+        array_message_state_pop_back(NULL, scene_state);
         superscene_pop_scene();
         state->repaint = true;
     }
@@ -28,7 +61,11 @@ static void scene_input_handler_message(struct state *state, int input)
 
 static void scene_paint_handler_message(struct state *state)
 {
-    font_print_centered(state->gs, scene_state.y, 1.0, FG, scene_state.text);
+    message_state_t *msg_state = array_message_state_back(scene_state);
+    if (!msg_state)
+        return;
+
+    font_print_centered(state->gs, msg_state->y, 1.0, FG, msg_state->text);
     superscene_clear_button_guide(state);
     state->button_guide.circle = L"Return";
     state->repaint = true;
@@ -36,8 +73,12 @@ static void scene_paint_handler_message(struct state *state)
 
 void scene_switch_to_message(struct state *state, wchar_t *text)
 {
-    scene_state.text = wcsdup(text);
-    scene_state.y = (state->gs->Height  / 2.0) - font_text_height(scene_state.text);
+    message_state_t msg_state = {
+        .text = wcsdup(text),
+        .y = (state->gs->Height  / 2.0) - font_text_height(text)
+    };
+
+    array_message_state_push_back(scene_state, msg_state);
 
     struct scene scene = {
         .input_handler = scene_input_handler_message,
